4b69b2bc8bb38282a14b55f41f9bd80522ef8f1f
Sgeo <sgeoster@gmail.com>
2020-12-22 17:10:01 -0500

    Redirect to localhost

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@
 static int (WINAPI * trueConnect)(SOCKET s, const sockaddr *name, int namelen) = connect;
 
 int WINAPI hookedConnect(SOCKET s, const sockaddr *name, int namelen) {
-    return SOCKET_ERROR;
     if(name->sa_family == AF_INET) {
         const sockaddr_in *name_in = reinterpret_cast<const sockaddr_in*>(name);
-        std::cout << "Port: " << ntohs(name_in->sin_port) << "\n";
+        std::cout << "Port: " << ntohs(name_in->sin_port) << "\n" << std::flush;
         if(name_in->sin_port == htons(1935)) {
-            std::cout << "Attempting to redirect 1935\n";
+            std::cout << "Attempting to redirect connect\n" << std::flush;
             sockaddr_in fake_name;
-            fake_name.sin_addr = name_in->sin_addr;
+            //fake_name.sin_addr = name_in->sin_addr;
+            fake_name.sin_addr.s_addr = inet_addr("127.0.0.1");
             fake_name.sin_family = name_in->sin_family;
-            fake_name.sin_port = htons(0);
-            return connect(s, (const sockaddr*)(&fake_name), sizeof(fake_name)); 
+            fake_name.sin_port = name_in->sin_port;
+            return trueConnect(s, (const sockaddr*)(&fake_name), sizeof(fake_name)); 
         }
     }
     return trueConnect(s, name, namelen);
